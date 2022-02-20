d5ad1c54b6ab66406e47b522348172b182c672b3
Dmitriy Kudlovich <carnagery@mail.ru>
2022-02-20 05:02:05 +0300

    save

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,37 @@
-console.log("Hello hey");
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  responsiveClass: true,
+  dots: false,
+
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    320: {
+      items: 3,
+      nav: true,
+      loop: false,
+      margin: 0,
+    },
+
+    768: {
+      items: 3,
+      nav: false,
+      loop: false,
+      margin: 0,
+    },
+
+    1440: {
+      loop: false,
+      items: 6,
+      margin: 0,
+    },
+
+    1920: {
+      items: 6,
+      loop: false,
+      margin: 0,
+    },
+  },
+});
